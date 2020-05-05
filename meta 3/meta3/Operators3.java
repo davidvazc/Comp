9e76740a752c9a2b6c8ec88fec7d1a5e838479ef
David Vaz Cortesão <davidvazc@gmail.com>
2020-05-05 11:54:22 +0100

    Add files via upload

---
+++ meta 3/meta3/Operators3.java
@@ -0,0 +1,20 @@
+class Sum {
+    public static void operator(){
+
+    }
+
+    public static void main() {
+        int a,b,c;
+        double d,e;
+        boolean x,y;
+
+        if(x==y){
+        	if(a==b){
+        		
+        	}
+        }
+
+        operator();
+    }
+
+}
